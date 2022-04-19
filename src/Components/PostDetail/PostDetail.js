8da4544f1da32e300a3225cdb827cd54eb7facb1
Hafizur Rahman Masum <32071354+hafiz07sec@users.noreply.github.com>
2022-04-19 23:15:57 +0200

    main

--- src/Components/PostDetail/PostDetail.js
+++ src/Components/PostDetail/PostDetail.js
@@ -11,13 +11,13 @@ const PostDetail = () => {
             fetch(url)
             .then(res => res.json())
             .then(data => setPost(data))
-    }, [])
+    }, [id])
     useEffect(() => {
             const url = `https://jsonplaceholder.typicode.com/comments?postId=${id}`;
             fetch(url)
             .then(res => res.json())
             .then(data => setComment(data))
-    }, [])
+    }, [id])
     return (
         <div>
             <h3>This is {id} post detail</h3>
